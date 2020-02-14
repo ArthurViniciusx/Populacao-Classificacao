e1928e949a742f9bd8fdbebeeb9ec39a206dc5a6
Arthur Vinícius <arthurviny1999@gmail.com>
2020-02-14 11:55:52 -0300

    Projeto Estudo/Populacao-Classificacao

---
+++ Populacao-Classificacao/src/main/java/com/arthurvinicius/cursomc/resource/PopulacaoResource.java
@@ -0,0 +1,82 @@
+package com.arthurvinicius.cursomc.resource;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arthurvinicius.cursomc.Dto.ClassificacaoDto;
+import com.arthurvinicius.cursomc.Dto.PopulacaoDto;
+import com.arthurvinicius.cursomc.domain.Populacao;
+import com.arthurvinicius.cursomc.repository.ClassificacaoRepository;
+import com.arthurvinicius.cursomc.repository.PopulacaoRepository;
+import com.arthurvinicius.cursomc.service.PopulacaoService;
+
+@RestController
+@RequestMapping(value = "/master/populacoes")
+public class PopulacaoResource {
+
+	@Autowired
+	PopulacaoRepository populacaoRepository;
+
+	@Autowired
+	ClassificacaoRepository classificacaoRepository;
+	
+	@Autowired
+	PopulacaoService populacaoService;
+	
+//	@GetMapping
+//	public ResponseEntity<List<?>> findAll() {
+//		return ResponseEntity.ok().body(populacaoRepository.findAll());
+//	}
+
+	@GetMapping
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<PopulacaoDto>> findAll() {
+		List<Populacao> list = populacaoService.findAll();
+		List<PopulacaoDto> listDto = list.parallelStream().map(obj -> new PopulacaoDto())
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findById(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(populacaoRepository.findById(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<PopulacaoDto> save(@RequestBody PopulacaoDto populacaoDto) {
+		populacaoService.inserirPopulacoes(populacaoDto.toObject());
+		return ResponseEntity.ok().body(populacaoDto);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Populacao populacao) {
+		isPrest(id);
+		return ResponseEntity.ok().body(populacaoRepository.save(populacao));
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Integer id) {
+		isPrest(id);
+		populacaoRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	public void isPrest(Integer id) {
+		if (!populacaoRepository.findById(id).isPresent()) {
+
+		}
+	}
+}
